b0e4c7ce1b6c67c9f72fe53696afc10a9ac426d9
Eom-JI-WOONG <33675101+Eom-JI-WOONG@users.noreply.github.com>
2017-11-15 16:48:01 +0900

    Update database.py
    서버 DB접속정보변경

--- module/database.py
+++ module/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('postgresql+psycopg2://test:1234@localhost/testdb', convert_unicode=True)
+engine = create_engine('postgresql+psycopg2://test:a454545!@localhost/test', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 Base = declarative_base()
 Base.query = db_session.query_property()
