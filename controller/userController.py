3e384f41fe7af193b8747be183b4ae6ac025c4bf
Eom-JI-WOONG <skyofblue12@naver.com>
2017-11-16 17:43:10 +0900

    등록 method 추가

--- controller/userController.py
+++ controller/userController.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 
-import json
+import json,datetime
 
 from models.userInfo import UserInfo
 from module.User import User
@@ -41,7 +41,22 @@ class userInfoService:
         querys = db_session.query(UserInfo)
         users={}
         for q in querys:
-            users[q.id] = User(q.id,q.passwd)
+            users[q.id] = User(q.id,q.passwd,q.name)
         print users
         return users
 
+    @staticmethod
+    def regist_User(userId,passwd,name):
+        print "Regist user method start!"
+        user = UserInfo(userId,passwd,name,datetime.datetime.now())
+        print user
+        try:
+            db_session.add(user)
+            db_session.commit()
+            print db_session.query(UserInfo).count()
+        except Exception as e:
+            print e.message
+            db_session.rollback()
+            raise e
+        finally:
+            db_session.close()
